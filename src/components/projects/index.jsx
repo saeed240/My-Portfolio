509fd2b5f6834b8031aea0b57b61c7326a510649
saeed240 <saeed12.contact@gmail.com>
2026-08-18 18:40:30 +0200

    Add some projects to my portfolio

--- src/components/projects/index.jsx
+++ src/components/projects/index.jsx
@@ -1,9 +1,24 @@
 import React from "react";
 
-export default function () {
+//internal import
+import "./styles.css";
+
+function Projects() {
   return (
-    <div>
-      <h1>My Projects</h1>
-    </div>
+    <>
+      <div className="project-container">
+        <h2>Projects_______</h2>
+
+        <div className="projects">
+          <a href="https://github.com/saeed240/My-Portfolio">My Portfolio</a>
+          <a href="https://github.com/saeed240/Nextcent_v1"> Landing Page</a>
+          <a href="https://github.com/saeed240/Book-Inventory">
+            {" "}
+            Book Inventory
+          </a>
+        </div>
+      </div>
+    </>
   );
 }
+export default Projects;
